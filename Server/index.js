969ee2b4cb8c24b5fc918d6191ad717fe8aa03e6
dmitriy2893 <dmitriy2893@gmail.com>
2021-12-03 13:06:41 +0300

    c

--- Server/index.js
+++ Server/index.js
@@ -16,8 +16,6 @@ app.use('/posts', postsRoute)
 
 const PORT = 5000
 
-
-
 app.listen(process.env.PORT || PORT, () => {
-	console.log(`Runn port ${PORT}!`);
+	console.log(`Runn port ${PORT}!!!!!!`);
 })
